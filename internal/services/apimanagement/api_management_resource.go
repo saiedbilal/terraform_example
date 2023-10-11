2789d661aa398b1cf9d041e1c1ef2d842c220180
Elena Xin <v-elenaxin@microsoft.com>
2023-10-11 14:54:04 +0800

    update code

--- internal/services/apimanagement/api_management_resource.go
+++ internal/services/apimanagement/api_management_resource.go
@@ -1193,15 +1193,19 @@ func resourceApiManagementServiceUpdate(d *pluginsdk.ResourceData, meta interfac
 			if err != nil {
 				return err
 			}
-
 			policyServiceId := policy.NewServiceID(subscriptionId, id.ResourceGroupName, id.ServiceName)
 			if policyContract != nil {
 				if _, err := policyClient.CreateOrUpdate(ctx, policyServiceId, *policyContract, policy.CreateOrUpdateOperationOptions{}); err != nil {
 					return fmt.Errorf(" setting Policies for %s: %+v", id, err)
 				}
-			} else if resp, err := policyClient.Delete(ctx, policyServiceId, policy.DeleteOperationOptions{}); err != nil {
-				if !response.WasNotFound(resp.HttpResponse) {
-					return fmt.Errorf("removing Policies from %s: %+v", id, err)
+			} else {
+				oldPolicy, newPolicy := d.GetChange("policy")
+				if len(oldPolicy.([]interface{})) > 0 && len(newPolicy.([]interface{})) == 0 {
+					if resp, err := policyClient.Delete(ctx, policyServiceId, policy.DeleteOperationOptions{}); err != nil {
+						if !response.WasNotFound(resp.HttpResponse) {
+							return fmt.Errorf("removing Policies from %s: %+v", id, err)
+						}
+					}
 				}
 			}
 		}
